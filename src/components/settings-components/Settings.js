d96afaab6b62d443e810efe173db330d329b08ae
Роман Казаков <roman.delarge@gmail.com>
2023-08-12 17:36:03 +0300

    added main components

---
+++ src/components/settings-components/Settings.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Settings = () => {
+    return (
+        <div>
+            <div>settings</div>
+            <button>OK</button>
+        </div>
+    )
+}
+
+export default Settings;
