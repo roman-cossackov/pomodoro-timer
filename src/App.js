6510879393450cce906d94a5a5b7d7994d02d988
Роман Казаков <roman.delarge@gmail.com>
2023-08-13 17:32:10 +0300

    Added settings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import React, {useState} from 'react';
 
-import './App.css';
+import './css/App.css';
 
 import Timer from './components/timer-components/Timer'
 import Settings from './components/settings-components/Settings.js'
+import SettingsContext from './components/settings-components/SettingsContext.js'
 
 
 
 function App() {
-
-    const [showSettings, setShowSettings] = useState(false)
+    const [showSettings, setShowSettings] = useState(false);
+    const [workMinutes, setWorkMinutes] = useState(45);
+    const [breakMinutes, setBreakMinutes] = useState(15);
 
     const turnOnSettingsHandler = () => {
       setShowSettings(true)
     }
 
+    const turnOffSettingsHandler = () => {
+        setShowSettings(false)
+    }
+
     return (
         <main>
-            {showSettings ? <Settings /> : <Timer onTurnOnSettings={turnOnSettingsHandler}/>}
+            <SettingsContext.Provider value={{
+                workMinutes,
+                breakMinutes,
+                setWorkMinutes,
+                setBreakMinutes,
+            }}>
+                {showSettings ? <Settings onTurnOffSettings={turnOffSettingsHandler}/> : <Timer onTurnOnSettings={turnOnSettingsHandler}/>}
+            </SettingsContext.Provider>
         </main>
     );
 }
